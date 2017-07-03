8ba26541ae56e28eeefb1583f192fec892741d37
levani <levanijincharadze@outlook.com>
2017-07-03 23:47:49 +0400

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GtuUniversity.Utilities
+{
+    public static class Utilities
+    {
+        public static string IsActive(HtmlHelper html,
+                                  string control,
+                                  string action)
+        {
+            var routeData = html.ViewContext.RouteData;
+
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+
+            // both must match
+            var returnActive = control == routeControl &&
+                               action == routeAction;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
